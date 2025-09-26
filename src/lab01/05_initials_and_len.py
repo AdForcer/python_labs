c604cdd2287ebe05f5a725722d3b6d14ae0387d9
AdForcer <vdkuziakin@gmail.com>
2025-09-26 11:30:35 +0300

    --

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,3 +1,3 @@
 Full_Name = input('ФИО: ')
 Full_Name = Full_Name.split()
-print(f'{Full_Name[0][0]}{Full_Name[1][0]}{Full_Name[2][0]}.\nДлина (символов): {sum([len(x) for x in Full_Name])}')
+print(f'{Full_Name[0][0]}{Full_Name[1][0]}{Full_Name[2][0]}.\nДлина (символов): {sum([len(x) for x in Full_Name])+2}')
